5c34de3f80346968023b06d8990bab2b7897abbe
debashishW <debashishdas404020@gmail.com>
2024-03-13 01:12:05 +0600

    react added

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,12 +1,10 @@
-
-
+import profile from '../../assets/images/profile.png'
 const Header = () => {
     return (
-      <>
-        <div>
-             <h1 className='text-4xl'>Knowlwdge Cafe</h1>
-        </div>
-      </>
+        <header className='flex justify-between items-center p-4 border-b-2  max-w-7xl mx-auto'>
+            <h1 className='text-4xl font-bold'>Knowledge Cafe</h1>
+            <img src={profile} alt="" />
+        </header>
     );
 };
 
