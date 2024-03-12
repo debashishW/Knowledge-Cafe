85147621895169782f33ac21796959a2373214ce
debashishW <debashishdas404020@gmail.com>
2024-03-12 21:18:16 +0600

    json file and file added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 
-
+import Header from './Components/Header/Header'
 import './App.css'
+import Blogs from './Components/Blogs/Blogs'
 
 function App() {
   
 
   return (
     <>
-      
-      <h1 className='text-xl bg-red-500'>Knowlwdge Cafe</h1>
+      <Header></Header>
+      <Blogs></Blogs>
       
     </>
   )
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,13 @@
+
+
+const Header = () => {
+    return (
+      <>
+        <div>
+             <h1 className='text-4xl'>Knowlwdge Cafe</h1>
+        </div>
+      </>
+    );
+};
+
+export default Header;
