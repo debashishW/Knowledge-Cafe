35424373e8f1a492900159a6864cbd2674a7af71
debashishW <debashishdas404020@gmail.com>
2024-03-12 20:52:55 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <h1 className='text-xl bg-red-500'>Knowlwdge Cafe</h1>
+      
+    </>
+  )
+}
+
+export default App
